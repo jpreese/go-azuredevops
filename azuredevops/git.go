eb2edc2cd6f64119374576b99d8c047d73e70c4a
David McPike <7037366+mcdafydd@users.noreply.github.com>
2019-04-18 01:43:20 -0500

    move git structs to git.go; remove duplicate GitRef struct

--- azuredevops/git.go
+++ azuredevops/git.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/url"
+	"time"
 )
 
 // GitService handles communication with the git methods on the API
@@ -18,12 +19,237 @@ type GitRefsResponse struct {
 	Refs  []GitStatus `json:"value"`
 }
 
-// GitRef describes what the git reference looks like
+// GitChange describes file path and content changes
+type GitChange struct {
+	ChangeID           *int                      `json:"changeId,omitempty"`
+	ChangeType         *VersionControlChangeType `json:"changeType,omitempty"`
+	Item               *GitItem                  `json:"item,omitempty"`
+	NewContent         *ItemContent              `json:"newContent,omitempty"`
+	NewContentTemplate *GitTemplate              `json:"newContentTemplate,omitempty"`
+	OriginalPath       *string                   `json:"originalPath,omitempty"`
+	SourceServerItem   *string                   `json:"sourceServerItem,omitempty"`
+	URL                *string                   `json:"url,omitempty"`
+}
+
+// GitCommitChanges is a list of GitCommitRefs and count of all changes describes in
+// the response from the API
+type GitCommitChanges struct {
+	ChangeCounts *ChangeCountDictionary `json:"changeCounts,omitempty"`
+	Changes      *[]GitChange           `json:"changes,omitempty"`
+}
+
+// GitCommitRef describes a single git commit reference
+type GitCommitRef struct {
+	Links            *[]ReferenceLinks      `json:"_links,omitempty"`
+	CommitID         *string                `json:"commitId,omitempty"`
+	Author           *GitUserDate           `json:"author,omitempty"`
+	Committer        *GitUserDate           `json:"committer,omitempty"`
+	Comment          *string                `json:"comment,omitempty"`
+	CommentTruncated *bool                  `json:"commentTruncated,omitempty"`
+	URL              *string                `json:"url,omitempty"`
+	ChangeCounts     *ChangeCountDictionary `json:"changeCounts,omitempty"`
+	Changes          *GitChange             `json:"changes,omitempty"`
+	Parents          *[]string              `json:"parents,omitempty"`
+	Push             *GitPushRef            `json:"push,omitempty"`
+	RemoteURL        *string                `json:"remoteUrl,omitempty"`
+	Statuses         *[]GitStatus           `json:"statuses,omitempty"`
+	WorkItems        *ResourceRef           `json:"workItems,omitempty"`
+}
+
+// GitRef provides information about a git/fork ref.
 type GitRef struct {
-	Name     string      `json:"name,omitempty"`
-	ObjectID string      `json:"objectId,omitempty"`
-	URL      string      `json:"url,omitempty"`
-	Statuses []GitStatus `json:"statuses,omitempty"`
+	Links          *[]ReferenceLinks `json:"_links,omitempty"`
+	Creator        *IdentityRef      `json:"creator,omitempty"`
+	IsLocked       *bool             `json:"isLocked,omitempty"`
+	IsLockedBy     *IdentityRef      `json:"isLockedBy,omitempty"`
+	Name           *string           `json:"name,omitempty"`
+	ObjectID       *string           `json:"objectId,omitempty"`
+	PeeledObjectID *string           `json:"peeledObjectId,omitempty"`
+	Repository     *GitRepository    `json:"repository,omitempty"`
+	Statuses       *[]GitStatus      `json:"statuses,omitempty"`
+	URL            *string           `json:"url,omitempty"`
+}
+
+// GitItem describes a single git item
+type GitItem struct {
+	Links                 *[]ReferenceLinks    `json:"_links,omitempty"`
+	CommitID              *string              `json:"commitId,omitempty"`
+	Content               *string              `json:"content,omitempty"`
+	ContentMetadata       *FileContentMetadata `json:"contentMetadata,omitempty"`
+	GitObjectType         *GitObjectType       `json:"gitObjectType,omitempty"`
+	IsFolder              *bool                `json:"isFolder,omitempty"`
+	IsSymLink             *bool                `json:"isSymLink,omitempty"`
+	LatestProcessedChange *GitCommitRef        `json:"latestProcessedChange,omitempty"`
+	ObjectID              *string              `json:"objectId,omitempty"`
+	OriginalObjectID      *string              `json:"originalObjectId,omitempty"`
+	Path                  *string              `json:"path,omitempty"`
+	URL                   *string              `json:"url,omitempty"`
+}
+
+// GitPullRequest represents all the data associated with a pull request.
+type GitPullRequest struct {
+	Links                 *[]ReferenceLinks                `json:"_links,omitempty"`
+	ArtifactID            *string                          `json:"artifactId,omitempty"`
+	AutoCompleteSetBy     *IdentityRef                     `json:"autoCompleteSetBy,omitempty"`
+	ClosedBy              *IdentityRef                     `json:"closedBy,omitempty"`
+	ClosedDate            *time.Time                       `json:"closedDate,omitempty"`
+	CodeReviewID          *int                             `json:"codeReviewId,omitempty"`
+	Commits               *[]GitCommitRef                  `json:"commits,omitempty"`
+	CompletionOptions     *GitPullRequestCompletionOptions `json:"completionOptions,omitempty"`
+	CompletionQueueTime   *time.Time                       `json:"completionQueueTime,	omitempty"`
+	CreatedBy             *IdentityRef                     `json:"createdBy,omitempty"`
+	CreationDate          *time.Time                       `json:"creationDate,omitempty"`
+	Description           *string                          `json:"description,omitempty"`
+	ForkSource            *GitRef                          `json:"forkSource,omitempty"`
+	IsDraft               *bool                            `json:"isDraft,omitempty"`
+	Labels                *WebAPITagDefinition             `json:"labels,omitempty"`
+	LastMergeCommit       *GitCommitRef                    `json:"lastMergeCommit,omitempty"`
+	LastMergeSourceCommit *GitCommitRef                    `json:"lastMergeSourceCommit,omitempty"`
+	LastMergeTargetCommit *GitCommitRef                    `json:"lastMergeTargetCommit,omitempty"`
+	MergeFailureMessage   *string                          `json:"mergeFailureMessage,omitempty"`
+	MergeFailureType      *PullRequestMergeFailureType     `json:"mergeFailureType,omitempty"`
+	MergeID               *string                          `json:"mergeId,omitempty"`
+	MergeOptions          *GitPullRequestMergeOptions      `json:"mergeOptions,omitempty"`
+	MergeStatus           *PullRequestAsyncStatus          `json:"mergeStatus,omitempty"`
+	PullRequestID         *int                             `json:"pullRequestId,omitempty"`
+	Repository            *GitRepository                   `json:"repository,omitempty"`
+	Reviewers             *[]IdentityRefWithVote           `json:"reviewers,omitempty"`
+	RemoteURL             *string                          `json:"remoteUrl,omitempty"`
+	SourceRefName         *string                          `json:"sourceRefName,omitempty"`
+	Status                *PullRequestStatus               `json:"status,omitempty"`
+	SupportsIterations    *bool                            `json:"supportsIterations,omitempty"`
+	TargetRefName         *string                          `json:"targetRefName,omitempty"`
+	Title                 *string                          `json:"title,omitempty"`
+	URL                   *string                          `json:"url,omitempty"`
+	WorkItemRefs          *[]ResourceRef                   `json:"workItemRefs,omitempty"`
+}
+
+// GitPullRequestCompletionOptions describes preferences about how the pull
+// request should be completed.
+type GitPullRequestCompletionOptions struct {
+	BypassPolicy            *bool                        `json:"bypassPolicy,omitempty"`
+	BypassReason            *string                      `json:"bypassReason,omitempty"`
+	DeleteSourceBranch      *bool                        `json:"deleteSourceBranch,omitempty"`
+	MergeCommitMessage      *int                         `json:"mergeCommitMessage,omitempty"`
+	MergeStrategy           *GitPullRequestMergeStrategy `json:"mergeStrategy,omitempty"`
+	SquashMerge             *bool                        `json:"squashMerge,omitempty"`
+	TransitionWorkItems     *bool                        `json:"transitionWorkItems,omitempty"`
+	TriggeredByAutoComplete *bool                        `json:"triggeredByAutoComplete,omitempty"`
+}
+
+// GitPullRequestMergeOptions describes the options which are used when a pull
+// request merge is created.
+type GitPullRequestMergeOptions struct {
+	DetectRenameFalsePositives *bool `json:"detectRenameFalsePositives,omitempty"`
+	DisableRenames             *bool `json:"disableRenames,omitempty"`
+}
+
+// GitPullRequestMergeStrategy specifies the strategy used to merge the pull request
+// during completion. If MergeStrategy is not set to any value, a no-FF merge will be
+// created if SquashMerge == false. If MergeStrategy is not set to any value, the pull
+// request commits will be squash if SquashMerge == true. The SquashMerge member is
+// deprecated. It is recommended that you explicitly set MergeStrategy in all cases.
+// If an explicit value is provided for MergeStrategy, the SquashMerge member will
+// be ignored.
+type GitPullRequestMergeStrategy struct {
+	NoFastForward *string `json:"noFastForward,omitempty"`
+	Rebase        *string `json:"rebase,omitempty"`
+	RebaseMerge   *string `json:"rebaseMerge,omitempty"`
+	Squash        *string `json:"squash,omitempty"`
+}
+
+// GitPush describes a code push request event.
+type GitPush struct {
+	Links      *[]ReferenceLinks `json:"_links,omitempty"`
+	Commits    *[]GitCommitRef   `json:"commits,omitempty"`
+	Date       *time.Time        `json:"date,omitempty"`
+	PushID     *int              `json:"pushId,omitempty"`
+	PushedBy   *IdentityRef      `json:"pushedBy,omitempty"`
+	RefUpdates *[]GitRefUpdate   `json:"refUpdates,omitempty"`
+	Repository *GitRepository    `json:"repository,omitempty"`
+	URL        *string           `json:"url,omitempty"`
+}
+
+// GitPushRef Describes a push request
+type GitPushRef struct {
+	Commits    *[]GitCommitRef `json:"commits,omitempty"`
+	RefUpdates *[]GitRefUpdate `json:"refUpdates,omitempty"`
+	Repository *GitRepository  `json:"repository,omitempty"`
+}
+
+// GitRefUpdate
+type GitRefUpdate struct {
+	IsLocked     *bool   `json:"isLocked,omitempty"`
+	Name         *string `json:"name,omitempty"`
+	NewObjectID  *string `json:"newObjectId,omitempty"`
+	OldObjectID  *string `json:"oldObjectId,omitempty"`
+	RepositoryID *string `json:"repositoryId,omitempty"`
+}
+
+// GitRepository describes an Azure Devops Git repository.
+type GitRepository struct {
+	Links            *[]ReferenceLinks     `json:"_links,omitempty"`
+	DefaultBranch    *string               `json:"defaultBranch,omitempty"`
+	ID               *string               `json:"id,omitempty"`
+	IsFork           *bool                 `json:"isFork,omitempty"`
+	Name             *string               `json:"name,omitempty"`
+	ParentRepository *GitRepositoryRef     `json:"parentRepository,omitempty"`
+	Project          *TeamProjectReference `json:"project,omitempty"`
+	RemoteURL        *string               `json:"remoteUrl,omitempty"`
+	Size             *int                  `json:"size,omitempty"`
+	SSHURL           *string               `json:"sshUrl,omitempty"`
+	URL              *string               `json:"url,omitempty"`
+	ValidRemoteURLs  *[]string             `json:"validRemoteUrls,omitempty"`
+}
+
+// GitRepositoryRef
+type GitRepositoryRef struct {
+	Collection *TeamProjectCollectionReference `json:"collection,omitempty"`
+	ID         *string                         `json:"id,omitempty"`
+	IsFork     *bool                           `json:"isFork,omitempty"`
+	Name       *string                         `json:"name,omitempty"`
+	Project    *TeamProjectReference           `json:"project,omitempty"`
+	RemoteURL  *string                         `json:"remoteUrl,omitempty"`
+	SSHURL     *string                         `json:"sshUrl,omitempty"`
+	URL        *string                         `json:"url,omitempty"`
+}
+
+// GitStatusState contains the metadata of a service/extension posting a status.
+type GitStatusState int
+
+// GitStatusState enum values
+const (
+	GitNotSet GitStatusState = iota
+	GitPending
+	GitSucceeded
+	GitFailed
+	GitError
+	GitNotApplicable
+)
+
+func (d GitStatusState) String() string {
+	return [...]string{"notSet", "pending", "succeeded", "failed", "error", "notApplicable"}[d]
+}
+
+type GitStatus struct {
+	Links        *[]ReferenceLinks `json:"_links,omitempty"`
+	Context      *GitStatusContext `json:"context,omitempty"`
+	CreatedBy    *IdentityRef      `json:"createdBy,omitempty"`
+	CreationDate *time.Time        `json:"creationDate,omitempty"`
+	Description  *string           `json:"description,omitempty"`
+	ID           *int              `json:"id,omitempty"`
+	State        *GitStatusState   `json:"state,omitempty"`
+	TargetURL    *string           `json:"targetUrl,omitempty"`
+	UpdatedDate  *time.Time        `json:"updatedDate,omitempty"`
+}
+
+// GitPullRequestStatus This class contains the metadata of a service/extension
+// posting pull request status. Status can be associated with a pull request or
+// an iteration.
+type GitPullRequestStatus struct {
+	GitStatus
+	IterationID int        `json:"iterationId,omitempty"`
+	Properties  *time.Time `json:"properties,omitempty"`
 }
 
 // GitRefListOptions describes what the request to the API should look like
@@ -31,6 +257,25 @@ type GitRefListOptions struct {
 	Filter             string `url:"filter,omitempty"`
 	IncludeStatuses    bool   `url:"includeStatuses,omitempty"`
 	LatestStatusesOnly bool   `url:"latestStatusesOnly,omitempty"`
+}
+
+// GitStatusContext Status context that uniquely identifies the status.
+type GitStatusContext struct {
+	Genre *string `json:"genre,omitempty"`
+	Name  *string `json:"name,omitempty"`
+}
+
+// GitTemplate
+type GitTemplate struct {
+	Name *string `json:"name,omitempty"`
+	Type *string `json:"type,omitempty"`
+}
+
+// GitUserDate User info and date for Git operations.
+type GitUserDate struct {
+	Name  *string    `json:"name,omitempty"`
+	Email *string    `json:"email,omitempty"`
+	Date  *time.Time `json:"date,omitempty"`
 }
 
 // ListRefs returns a list of the references for a git repo
